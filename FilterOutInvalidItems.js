790d6819aaeb6114c8f073d01f738e02a176495b
BALACHELLAMMA <balachellamma.v@numentica-ui.com>
2023-10-24 15:26:22 +0530

    type check code added

--- FilterOutInvalidItems.js
+++ FilterOutInvalidItems.js
@@ -8,16 +8,23 @@
 // Output [1, true, 33];
 
 function filterOutInvalidItemsInArray(inputArray,type){
+    if(inputArray.length==0 || type.length == 0) return 'Empty array or type not accepted'
     const result =[];
+    // const typeArray = ['boolean','number','string','bigint','object','undefined','function']
 
     for (let index=0; index<inputArray.length; index++){
 
         const currentCharacter = inputArray[index];
         if (typeof currentCharacter!== type) result.push(currentCharacter);
     }
+
     return result;
 }
 
-const type ='boolean';
-const array = ["a", "d", 1, true, "cd",33];
+const type ='boolean'
+const array = ["a", "d", 1, true, "cd", 33, undefined, null, ()=>{let name="bala"}, [1,2,3]];
 console.log(filterOutInvalidItemsInArray(array,type));
+console.log(filterOutInvalidItemsInArray(array,'object'));
+console.log(filterOutInvalidItemsInArray(array,'function'));
+
+
